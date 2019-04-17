a2ccb13d1c9ada6f74890c7c56fd197c8bf623cf
Adam Hoang <adamhoang712@gmail.com>
2019-04-16 17:04:03 -0700

    added more algos

--- leetalgos.js
+++ leetalgos.js
@@ -2636,3 +2636,23 @@
 // };
 // console.log(checkPerfectNumber(28));
 
+var compress = function(chars) {
+    chars.push("end");
+    let count = 1;
+    for(let i = 0; i< chars.indexOf("end"); i++){    
+        if(chars[i] === chars[i+1]){
+            count++;
+            continue;
+        }
+        chars.push(chars[i]);
+        if(count!==1){
+            chars.push(...(count.toString().split("")));
+        }
+        count = 1;
+    }
+   chars.splice(0,chars.indexOf("end")+1);
+   console.log(chars)
+};
+compress(["a","a","b","b","c","c","c"]);
+compress(["a","b","b","b","b","b","b","b","b","b","b","b","b"]);
+
