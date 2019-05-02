764d106f340927fbd0e69190ed1e919a527fe8b6
Adam Hoang <adamhoang712@gmail.com>
2019-05-02 15:34:06 -0700

    added more algos

--- leetalgos.js
+++ leetalgos.js
@@ -3350,3 +3350,61 @@
 // console.log(pivotIndex([1, 7, 3, 6, 5, 6]));
 // console.log(pivotIndex([1, 2, 3]));
 
+// var selfDividingNumbers = function(left, right) {
+// 	const res = [];
+//     for (let i=left; i<=right; i++) {
+// 		let str = i.toString();
+// 		let check = true;
+// 		for (let j=0, length= str.length; j<length; j++) {
+// 			if (i % parseInt(str[j]) !== 0) {
+// 				check = false;
+// 				break;
+// 			}
+// 		}
+// 		if (check === true) {
+// 			res.push(i);
+// 		}
+// 	}
+// 	return res;
+// };
+// console.log(selfDividingNumbers(1, 22));
+
+// var calculate = function (s) {
+//     let res = 0;
+//     let sign = 1;
+//     const stack = [];
+//     const opStack = [];
+
+//     for (let i=0, length=s.length; i<length; i++) {
+//         const char = s[i];
+//         if (char === " ") {
+//             continue;
+//         } else if (char === "+") {
+//             sign = 1;
+//         } else if (char === "-") {
+//             sign = -1;
+//         } else if (char >= "0" && char <= "9") {
+//             let num = char;
+//             while (i+1<length && s[i + 1] >= "0" && s[i + 1] <= "9") {
+//                 num += s[i + 1];
+//                 i++;
+//             }
+//             res += sign * parseInt(num);
+//         } else if (char === "(") {
+//             stack.push(res);
+//             res = 0;
+//             opStack.push(sign);
+//             sign = 1;
+//         } else if (char === ")") {
+//             res = opStack.pop() * res + stack.pop();
+//             sign = 1;
+//         }
+//     }
+//     return res;
+// };
+// console.log(calculate(" 1 + 1 "));
+// console.log(calculate(" 2-1 + 2 "));
+// console.log(calculate(" 8 - (2+3+1) + 2 "));
+// console.log(calculate("(1+(4+5+2)-3)+(6+8)"));
+
+
